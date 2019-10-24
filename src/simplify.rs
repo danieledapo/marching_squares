ae883b90320d798d973e9ca8c09e453e5589408d
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-10-24 21:58:06 +0200

    simplify: avoid allocations

--- src/simplify.rs
+++ src/simplify.rs
@@ -5,24 +5,29 @@ pub fn simplify(poly: &[(f64, f64)]) -> Vec<(f64, f64)> {
 }
 
 pub fn simplify_with_eps(poly: &[(f64, f64)], eps: f64) -> Vec<(f64, f64)> {
+    let mut r = vec![];
+    _simplify_with_eps(&mut r, poly, eps);
+    r
+}
+
+pub fn _simplify_with_eps(r: &mut Vec<(f64, f64)>, poly: &[(f64, f64)], eps: f64) {
     // Ramer Douglas Peucker doesn't work with closed paths, thus simplify the open path and then
     // close it manually
     if !poly.is_empty() && poly[0] == poly[poly.len() - 1] {
-        let mut s = rdp(&poly[..poly.len() - 1], eps);
-
-        s.push(poly[poly.len() - 1]);
-        s
+        rdp(r, &poly[..poly.len() - 1], eps);
+        r.push(poly[poly.len() - 1]);
     } else {
-        rdp(poly, eps)
+        rdp(r, poly, eps);
     }
 }
 
 /// Implementation of the [Ramer–Douglas–Peucker algorithm] to simplify an open path.
 ///
 /// [0]: https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
-fn rdp(poly: &[(f64, f64)], eps: f64) -> Vec<(f64, f64)> {
+fn rdp(r: &mut Vec<(f64, f64)>, poly: &[(f64, f64)], eps: f64) {
     if poly.len() < 3 {
-        return poly.to_vec();
+        r.extend_from_slice(poly);
+        return;
     }
 
     let sp = poly[0];
@@ -39,16 +44,15 @@ fn rdp(poly: &[(f64, f64)], eps: f64) -> Vec<(f64, f64)> {
     }
 
     if max_dist > eps {
-        let mut simplified = rdp(&poly[..=farthest_i], eps);
+        rdp(r, &poly[..=farthest_i], eps);
 
         // remove point with max dist, it will be added with the right vec
-        simplified.pop();
+        r.pop();
 
-        simplified.extend_from_slice(&simplify_with_eps(&poly[farthest_i..], eps));
-
-        simplified
+        _simplify_with_eps(r, &poly[farthest_i..], eps);
     } else {
-        vec![sp, ep]
+        r.push(sp);
+        r.push(ep);
     }
 }
 
