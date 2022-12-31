69450619d1cd3ba55054bdc70873f2ceb81f2d40
Daniele D'Orazio <daniele@develer.com>
2022-12-31 17:39:46 +0100

    remove boundary checks, it's useless

--- src/lib.rs
+++ src/lib.rs
@@ -79,26 +79,6 @@ pub fn march(field: &impl Field, z: f64) -> Contours {
             if ulz > z {
                 case |= 8;
             }
-
-            // if we're at the boundary of the image, consider solid squares as
-            // borders in order to close paths
-            case = match case {
-                15 => {
-                    if x == 0 {
-                        6
-                    } else if x == width - 2 {
-                        9
-                    } else if y == 0 {
-                        3
-                    } else if y == height - 2 {
-                        12
-                    } else {
-                        case
-                    }
-                }
-                _ => case,
-            };
-
             let x = x as f64;
             let y = y as f64;
 
